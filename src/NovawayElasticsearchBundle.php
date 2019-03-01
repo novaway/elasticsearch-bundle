ad668533f8a7f7ba515760c581923d25f6cebfe2
Jean-Pascal Devierne <jean-pascal@novaway.fr>
2019-03-01 16:32:17 +0100

    Add NovawayElasticSearchBundle

---
+++ src/NovawayElasticsearchBundle.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Novaway\ElasticsearchBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NovawayElasticsearchBundle extends Bundle
+{
+
+}
